d0d222726471f8f15f4089b1f08f713c877e77cf
Sam Ruby <rubys@intertwingly.net>
2024-11-29 18:03:44 -0500

    more startup pains

--- app/controllers/atc_controller.rb
+++ app/controllers/atc_controller.rb
@@ -1,5 +1,10 @@
 class AtcController < ApplicationController
   def index
-    @config = YAML.load_file(Rails.root + "config/atc.yml")
+    config_file = Rails.root + "config/atc.yml"
+    if File.exist? config_file
+      @config = YAML.load_file(Rails.root + "config/atc.yml")
+    else
+      redirect_to products_url
+    end
   end
 end
